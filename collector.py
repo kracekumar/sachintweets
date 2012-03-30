95e9ba7bbb95a15ab2d71f42916fd037fa836258
kracekumar <kracethekingmaker@gmail.com>
2012-03-30 06:59:08 +0530

    made some changes

--- collector.py
+++ collector.py
@@ -20,48 +20,26 @@ except Exception as e:
     log_handler.write(e.message)
 
 
-##################################### worker which pulls tweets ################
-def worker():
-    try:
-        context = zmq.Context(1)
-        receiver = context.socket(zmq.SUB)
-        receiver.connect("tcp://127.0.0.1:9999")
-        receiver.setsockopt(zmq.SUBSCRIBE, "t")
-        while 1:
-             [address, d] = receiver.recv_multipart()
-             d = json.loads(d)
-             try:
-                 if tweet:
-                     tweet.insert({'text': d['text'],\
-                       'location': d['user']['location'], \
-                       'uid': d['user']['id'], 'tid': d['id'],\
-                       'created_at': d['user']['created_at'],\
-                       'username': d['user']['name'],\
-                       'retweet_count': d['retweet_count']})
-             except TypeError as e:
-                 log_handler.write(MongoException(e))
-             except ValueError as e:
-                 log_handler.write(MongoException(e))
 
-    except Exception as e:
-        log_handler.write(e.message)
 
 def store_live_tweets():
     try:
-        Process(target=worker, args=()).start()
-        context = zmq.Context(1)
-        sender = context.socket(zmq.PUB)
-        sender.bind("tcp://*:9999")
         track = ['sachin', 'sachinism', 'Sachin', 'Sachinism', 'tendulkar', 'Tendulkar', 'sachintendulkar','SachinTendulkar']
         while True:
             r = requests.post('https://stream.twitter.com/1/statuses/filter.json',
                     data={'track': track}, auth=(username, password))
             for line in r.iter_lines():
                 if line:
-                    sender.send_multipart(["t", line])
-                    print "===sent==="
+                    if tweet:
+                        tweet.insert({'text': line['text'],\
+                       'location': line['user']['location'], \
+                       'uid': line['user']['id'], 'tid': line['id'],\
+                       'created_at': line['user']['created_at'],\
+                       'username': line['user']['name'],\
+                       'retweet_count': line['retweet_count']})
+                        print "===added to db==="
             else:
-                time.sleep(60)            
+                time.sleep(60)
     except Exception as e:
         log_handler.write(e.message)
 
