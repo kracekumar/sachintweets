f670f2b1c7a1f402c6029edff4309a82b57308f6
kracekumar <kracethekingmaker@gmail.com>
2012-04-01 00:30:58 +0530

    added shell script to start collector.py and recv.py

---
+++ start.sh
@@ -0,0 +1,8 @@
+<<ABOUT
+    shell script to start collector.py and recv.py
+    primarily written to save typing
+    this file will also be executed using nohup ./start.sh&
+ABOUT
+nohup python2.7 collector.py&
+nohup python2.7 recv.py&
+
